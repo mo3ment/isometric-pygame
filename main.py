9e1b3e243c16562bb0a0ed5640b20d2e968aa9a9
acke80 <stoffe.akr@hotmail.com>
2023-11-21 17:15:10 +0100

    Add variable timestep, for butter smooth movement, however not so smooth due to pygame

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pygame, sys
 import numpy as np
 from pygame.locals import *
-
-
+from time import time_ns
 #=====================================================
 #==================== FUNCTIONS ======================
 #=====================================================
@@ -17,6 +16,7 @@ WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 
+PLAYER_VEL = 40
 
 #=====================================================
 #=================== GAME WINDOW =====================
@@ -28,11 +28,9 @@ pygame.display.set_caption('LIBERATION')
 screen = pygame.display.set_mode((960, 720),0,32)
 display = pygame.Surface((400, 300))
 
-
 #=====================================================
 #===================== FUNCTIONS =====================
 #=====================================================
-
 
 def collision_test(rect,tiles):
     collisions = []
@@ -42,83 +40,83 @@ def collision_test(rect,tiles):
             return collisions
     return collisions
  
-def check_collision_x(map_rect,iso_rect,tiles,left,right,up,down):
+def check_collision_x(_map_x, _map_y, _iso_x, _iso_y, w, h, tiles,left,right,up,down, vel):
     corner_case = False
-    
+
     if (up and left) or (up and right):
         corner_case = True
-        map_rect.x -= 1*up
-        iso_rect.x -= 2*up
-        iso_rect.y -= 1*up
+        _map_x -= 1*up * dt * vel
+        _iso_x -= 2*up * dt * vel
+        _iso_y -= 1*up * dt * vel
 
     elif (left and down) or (right and down):
         corner_case = True
-        map_rect.x += 1*down
-        iso_rect.x += 2*down
-        iso_rect.y += 1*down
+        _map_x += 1*down * dt * vel
+        _iso_x += 2*down * dt * vel
+        _iso_y += 1*down * dt * vel
 
     else:
-        map_rect.x -= 2*(up - down)
-        iso_rect.x -= 4*(up - down)
-        iso_rect.y -= 2*(up - down)
-    collisions = collision_test(map_rect,tiles)
+        _map_x -= 2*(up - down) * dt * vel
+        _iso_x -= 4*(up - down) * dt * vel
+        _iso_y -= 2*(up - down) * dt * vel
+    collisions = collision_test(pygame.Rect((_map_x, _map_y), (w, h)),tiles)
 
     for tile in collisions:
         if down:
             if corner_case:
-                map_rect.x -= 1
-                iso_rect.x -= 2
-                iso_rect.y -= 1 
+                _map_x -= 1 * dt * vel
+                _iso_x -= 2 * dt * vel
+                _iso_y -= 1 * dt * vel 
             else:
-                map_rect.x -= 2
-                iso_rect.x -= 4
-                iso_rect.y -= 2
+                _map_x -= 2 * dt * vel
+                _iso_x -= 4 * dt * vel
+                _iso_y -= 2 * dt * vel
         elif up:
             if corner_case:
-                map_rect.x += 1
-                iso_rect.x += 2
-                iso_rect.y += 1 
+                _map_x += 1 * dt * vel
+                _iso_x += 2 * dt * vel
+                _iso_y += 1 * dt * vel
             else:
-                map_rect.x += 2
-                iso_rect.x += 4
-                iso_rect.y += 2
-    return map_rect, iso_rect
+                _map_x += 2 * dt * vel
+                _iso_x += 4 * dt * vel
+                _iso_y += 2 * dt * vel
+    return _map_x, _map_y, _iso_x, _iso_y
 
-def check_collision_y(map_rect,iso_rect,tiles,left,right,up,down):
+def check_collision_y(_map_x, _map_y, _iso_x, _iso_y, w, h, tiles,left,right,up,down, vel):
     corner_case = False
 
     if (left and up) or (right and up) or (left and down) or (right and down):
         corner_case = True
-        map_rect.y -= 1*(right - left)
-        iso_rect.x += 2*(right - left)
-        iso_rect.y -= 1*(right - left)
+        _map_y -= 1*(right - left) * dt * vel
+        _iso_x += 2*(right - left) * dt * vel
+        _iso_y -= 1*(right - left) * dt * vel
 
     else:
-        map_rect.y -= 2*(right - left)
-        iso_rect.x += 4*(right - left)
-        iso_rect.y -= 2*(right - left)
-    collisions = collision_test(map_rect,tiles)
+        _map_y -= 2*(right - left) * dt * vel
+        _iso_x += 4*(right - left) * dt * vel
+        _iso_y -= 2*(right - left) * dt * vel
+    collisions = collision_test(pygame.Rect((_map_x, _map_y), (w, h)),tiles)
 
     for tile in collisions:
         if left:
             if corner_case:
-                map_rect.y -= 1
-                iso_rect.x += 2
-                iso_rect.y -= 1
+                _map_y -= 1 * dt * vel
+                _iso_x += 2 * dt * vel
+                _iso_y -= 1 * dt * vel
             else:
-                map_rect.y -= 2
-                iso_rect.x += 4
-                iso_rect.y -= 2
+                _map_y -= 2 * dt * vel
+                _iso_x += 4 * dt * vel
+                _iso_y -= 2 * dt * vel
         elif right:
             if corner_case:
-                map_rect.y += 1
-                iso_rect.x -= 2
-                iso_rect.y += 1
+                _map_y += 1 * dt * vel
+                _iso_x -= 2 * dt * vel
+                _iso_y += 1 * dt * vel
             else:
-                map_rect.y += 2
-                iso_rect.x -= 4
-                iso_rect.y += 2
-    return map_rect, iso_rect
+                _map_y += 2 * dt * vel
+                _iso_x -= 4 * dt * vel
+                _iso_y += 2 * dt * vel
+    return _map_x, _map_y, _iso_x, _iso_y
 
 #=====================================================
 #==================== INITIATIONS ====================
@@ -138,19 +136,30 @@ player_shadow_sprite = pygame.image.load('player-shadow-upscaled.png')
 player_shadow_sprite.set_colorkey(RED)
 player_shadow_rect = pygame.Rect((player_map_rect.x/MAP_RATE-player_map_rect.y/MAP_RATE)*(16-2)+7*UPSCALE-1, (player_map_rect.y/MAP_RATE+player_map_rect.x/MAP_RATE)*(8-1)+1*UPSCALE,player_shadow_sprite.get_width(),player_shadow_sprite.get_height())
 
+_map_x = player_map_rect.x
+_map_y = player_map_rect.y
+_iso_x = player_shadow_rect.x
+_iso_y = player_shadow_rect.y
+
 true_camera = [0,0]
 
 #=====================================================
 #==================== GAME-LOOP ======================
 #=====================================================
 
+last_time = time_ns()
 while True:
+    now = time_ns()
+    dt = (now - last_time) * 1e-9
+    #print(int(1.0 / dt) if dt != 0 else "0")
+    last_time = now
+
     # clear display #
     boundary_tiles = []
-    display.fill(WHITE)
+    display.fill(BLACK)
 
-    true_camera[0] += (player_shadow_rect.centerx-true_camera[0]-100*UPSCALE)/10
-    true_camera[1] += (player_shadow_rect.centery-true_camera[1]-75*UPSCALE)/10
+    true_camera[0] += (player_shadow_rect.centerx-true_camera[0]-100*UPSCALE)/10 * dt * PLAYER_VEL
+    true_camera[1] += (player_shadow_rect.centery-true_camera[1]-75*UPSCALE)/10 * dt * PLAYER_VEL
     camera = true_camera.copy()
     camera[0] = int(camera[0])
     camera[1] = int(camera[1])
@@ -170,17 +179,14 @@ while True:
 
     player_velocity = np.array([0,0], dtype=np.int8)
 
-    if right == True:
-        player_map_rect, player_shadow_rect = check_collision_y(player_map_rect,player_shadow_rect,boundary_tiles,left,right,up,down)
-    if left == True:
-        player_map_rect, player_shadow_rect = check_collision_y(player_map_rect,player_shadow_rect,boundary_tiles,left,right,up,down)
-    if up == True:
-        player_map_rect, player_shadow_rect = check_collision_x(player_map_rect,player_shadow_rect,boundary_tiles,left,right,up,down)
-    if down == True:
-        player_map_rect, player_shadow_rect = check_collision_x(player_map_rect,player_shadow_rect,boundary_tiles,left,right,up,down)
-        
+    _map_x, _map_y, _iso_x, _iso_y = check_collision_x(_map_x, _map_y, _iso_x, _iso_y, player_map_rect.w, player_map_rect.h, boundary_tiles,left,right,up,down, PLAYER_VEL)
+    _map_x, _map_y, _iso_x, _iso_y = check_collision_y(_map_x, _map_y, _iso_x, _iso_y, player_map_rect.w, player_map_rect.h, boundary_tiles,left,right,up,down, PLAYER_VEL)
 
-    
+    player_map_rect.x = _map_x
+    player_map_rect.y = _map_y
+    player_shadow_rect.x = _iso_x
+    player_shadow_rect.y = _iso_y
+
     #pygame.draw.rect(display,WHITE,player_map_rect)
     #pygame.draw.rect(display,RED,player_shadow_rect)
     display.blit(player_shadow_sprite, (player_shadow_rect.x-camera[0],player_shadow_rect.y-camera[1]))
@@ -215,4 +221,4 @@ while True:
     # update display #
     screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
     pygame.display.update()
-    game_clock.tick(FPS)
+    #game_clock.tick(FPS)
